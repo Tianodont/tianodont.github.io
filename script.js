b35674401e936f51cec742aa5198d778a8739050
Tianodont <104167316+Tianodont@users.noreply.github.com>
2023-02-28 10:33:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 
+fetch("/", {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify({
+                    "massive": "1"
+                })
+            })
+                .then(function (response) {
+                    if (response.ok) {
+                        //pass
+                    }
+                    else {
+                        throw Error('Something went wrong');
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+fetch("/", {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: ' GET',
+                body: JSON.stringify({
+                    "massive": "1
+                })
+            })
+                .then(function (response) {
+                    if (response.ok) {
+                        alert("2")
+                    }
+                    else {
+                        throw Error('Something went wrong');
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
