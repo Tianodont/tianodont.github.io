ff060f891298ae8f7e3b1b552dde9fa50b8cdc22
Tianodont <104167316+Tianodont@users.noreply.github.com>
2023-02-28 10:47:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,30 +3,19 @@ const button = document.createElement('button')
 // Set the button text to 'Can you click me?'
 button.innerText = 'Can you click me?'
 button.addEventListener('click', () => {
-  alert(fetch("https://tianodont.github.io/"))
+  alert(fetch("https://tianodont.github.io/",  {               
+        headers: {
+                    'Content-Type': 'application/json'
+                },
+        method: ' GET',
+        body: {"1" : "2"}
+                }
+       )
   
 })
 
 
 
+let t = await fetch("https://tianodont.github.io/")
 
-fetch("/", {
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                method: ' GET',
-                body: JSON.stringify({
-                    "massive": "1
-                })
-            })
-                .then(function (response) {
-                    if (response.ok) {
-                        alert("2")
-                    }
-                    else {
-                        throw Error('Something went wrong');
-                    }
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
+alert(t)
