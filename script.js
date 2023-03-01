7a00d2996fbdb4ee7b71d88f2f2f07507b040df7
Tianodont <tianodont@gmail.com>
2023-03-01 21:56:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,31 @@ const button = document.createElement('button')
 // Set the button text to 'Can you click me?'
 button.innerText = 'Can you click me?'
 button.addEventListener('click', () => {
-  alert(fetch("https://tianodont.github.io/",  {               
-        headers: {
-                    'Content-Type': 'application/json'
+        fetch("https://tianodont.github.io/", {
+                method: "post",
+                headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
                 },
-        method: ' GET',
-        body: {"1" : "2"}
-                }
-       )
-  
+                body: JSON.stringify({
+                        "1": "2",
+                        "2": "3"
+                })
+        })
+                .then((response) => {
+                        //do something awesome that makes the world a better place
+                });
+
 })
 
 
-
-let t = await fetch("https://tianodont.github.io/").json()
-
-alert(t)
+try {
+        const response = await fetch('https://tianodont.github.io/', {
+        method: 'POST',
+        body: formData
+        });
+        const result = await response.json();
+        console.log('Успех:', JSON.stringify(result));
+        } catch (error) {
+        console.error('Ошибка:', error);
+        }
